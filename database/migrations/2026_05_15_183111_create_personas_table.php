e7b5582c18e388f9f276b1ffddc397cdc1d9eda6
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-05-18 09:46:38 -0600

    cambios migracion

--- database/migrations/2026_05_15_183111_create_personas_table.php
+++ database/migrations/2026_05_15_183111_create_personas_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('personas', function (Blueprint $table) {
             $table->id();
+
+            $table->string('codigo', 50)->nullable();
+            $table->string('marca_temporal', 50)->nullable();
+            $table->string('dpi_cui', 20)->nullable();
+            $table->string('primer_nombre', 100)->nullable();
+            $table->string('segundo_nombre', 100)->nullable();
+            $table->string('primer_apellido', 100)->nullable();
+            $table->string('segundo_apellido', 100)->nullable();
+            $table->string('telefono_de_contacto', 20)->nulllable();
+            $table->string('correo_electronico', 150)->nullable();
+            $table->string('fecha_de_nacimiento', 50)->nullable();
             $table->integer('edad')->nullable();
             $table->enum('sexo', ['M', 'F'])->nullable();
 
